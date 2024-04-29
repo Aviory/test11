bf23257db796535e7008396fdc2bd1048af523c2
Vel <avior@gmail.com>
2024-04-29 19:35:09 +0300

    добавив логин пароль

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Main  {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+//git init - створюе проеект
+//git add . - добавити файли для гіту за якими він слідкуе
+//git commit -m "добавив логин пароль"
