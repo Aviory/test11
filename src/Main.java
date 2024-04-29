677f133429ec84fe0b7d5b2129ba10228dd7d97d
Vel <avior@gmail.com>
2024-04-29 19:51:38 +0300

    some change

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.awt.event.KeyListener;
 
 public class Main  {
     public static void main(String[] args) {
-
+        System.out.println("hi");
     }
 
 
 }
 //git init - створюе проеект
 //git add . - добавити файли для гіту за якими він слідкуе
-//git commit -m "добавив логин пароль"
+//git commit -m "добавив логин пароль" - це зберігання проекту
+//git remote add origin https://github.com/Aviory/test11.git
